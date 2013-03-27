a4fde4f1c27180760c11a294281ac21be822dce7
lansdelen <max.romlin@svenskmedia.se>
2013-03-27 16:08:24 +0100

    alla filer i temat

---
+++ parts/shared/footer.php
@@ -0,0 +1,6 @@
+</div><!--end wrapper-->	
+	<footer>
+		<div class="wrapper">
+		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+		</div>
+	</footer>
---
+++ parts/shared/header.php
@@ -0,0 +1,12 @@
+<header>
+	<div class="wrapper">
+		<div id="logo">
+		<a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo( 'template_url' ); ?>/img/logo.png" /></a>
+		</div>
+	<div id="navigation">
+		<?php wp_nav_menu();?>
+	</div>
+	</div><!--end wrapper-->
+</header>
+
+<div class="wrapper">
